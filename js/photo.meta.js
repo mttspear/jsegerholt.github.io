8481762d057d465b519f083634770716d99ccfb7
Aderinsola <derin_akintilo@ziba.com>
2015-11-10 11:18:40 -0800

    adding necessary images. on g15

---
+++ js/photo.meta.js
@@ -0,0 +1,224 @@
+var ids = {
+    "#g01" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/1/1.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/1/1.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/1/1.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/1/1.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/1/1.5.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g02" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.5.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.6.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.7.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.8.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/2/2.9.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g03" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/3/3.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/3/3.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/3/3.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/3/3.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/3/3.5.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/3/3.6.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g04" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.5.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.6.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.7.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.8.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.9.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.10.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/4/4.11.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g05" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/5/5.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/5/5.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/5/5.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/5/5.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/5/5.5.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g06" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/6/6.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/6/6.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/6/6.3.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g07" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/7/7.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/7/7.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/7/7.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/7/7.4.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g08" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/8/8.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/8/8.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/8/8.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/8/8.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/8/8.5.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    },
+
+    "#g09" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.5.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.6.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.7.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.8.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.9.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.10.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.11.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/9/9.12.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g10" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/10/10.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/10/10.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/10/10.3.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g11" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/11/11.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/11/11.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/11/11.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/11/11.4.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+
+    "#g12" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/12/12.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/12/12.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/12/12.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/12/12.4.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+    "#g13" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/13/13.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/13/13.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/13/13.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/13/13.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/13/13.5.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+    
+        "#g14" : {
+        images: [ '<div class="col-xs-12 col-md-9 examples">' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/15/15.1.jpg" alt="">  </div>' + 
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/15/15.2.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/15/15.3.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/15/15.4.jpg" alt="">  </div>' +
+                 '<div class="col-xs-12 bounds"> <img class="img-responsive" src="imgs/15/15.5.jpg" alt="">  </div>' +
+                 '</div>' ], 
+        description: '<div id="no-pad" class="col-md-2">' +
+        '<p> Nike Sportswear </p>' +
+        '<p> Fall 2015 </p>' + 
+        '<p> Photo: Billy Kidd </p>' + 
+        '</div>'
+    }, 
+
+}
